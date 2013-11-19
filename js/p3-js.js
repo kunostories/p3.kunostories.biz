8cc43a4deb850d0b4a302bc55bd8c1efe5e83fec
Shawn Roe <roe.shawn@gmail.com>
2013-11-19 14:13:06 +0900

    Changed from dropdown to draggable and droppable pics.

--- js/p3-js.js
+++ js/p3-js.js
@@ -1,6 +1,6 @@
-/*-------------------------------------------------------------------------------------------------
-Get calorie value (title)
--------------------------------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------
+Add up calories from each food item
+----------------------------------------------------------------------*/
 $(document).ready(function() {
 	$('#foods').change(function() {
 		var foodCalories = parseInt($('#items').val());
@@ -8,5 +8,31 @@ $(document).ready(function() {
 		$('#items').prop('selectedIndex',0);
 		$('#totalCalories').text(totalCalories + foodCalories);
 
+	});
+});
+
+/*----------------------------------------------------------------------
+Pictures are draggable
+-----------------------------------------------------------------------*/
+$(document).ready(function() {
+	var contents = '';
+	var calories = '';
+	$('.food').draggable({
+		revert: true,
+		start: function() {
+			contents = $(this).attr('title');
+			calories = $(this).data('calories');
+		}
+	});
+
+	$('#canvas').droppable( {
+		accept: 'img',
+		drop: function() {
+			$('#plate').append('<li>' + contents + '</li>');
+
+			var totalCalories = parseInt($('#totalCalories').text());
+			var foodCalories = parseInt(calories);
+			$('#totalCalories').text(totalCalories + foodCalories);
+		}
 	});
 });
