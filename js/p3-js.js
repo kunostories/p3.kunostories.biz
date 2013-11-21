a83fd19a9478e112c8145ebaa05b280a2f7ad558
Shawn Roe <roe.shawn@gmail.com>
2013-11-21 16:45:12 +0900

    Added instructions, and testing mobile js

--- js/p3-js.js
+++ js/p3-js.js
@@ -56,3 +56,36 @@ $(document).ready(function() {
 	    heightStyle: "content"
 	});
 });
+
+
+/*----------------------------------------------------------------------
+Attempt to make the app 'mobile' friendly
+Code from http://stackoverflow.com/questions/5186441/javascript-drag-and-drop-for-touch-devices
+-----------------------------------------------------------------------*/
+function touchHandler(event) {
+    var touch = event.changedTouches[0];
+
+    var simulatedEvent = document.createEvent("MouseEvent");
+        simulatedEvent.initMouseEvent({
+        touchstart: "mousedown",
+        touchmove: "mousemove",
+        touchend: "mouseup"
+    }[event.type], true, true, window, 1,
+        touch.screenX, touch.screenY,
+        touch.clientX, touch.clientY, false,
+        false, false, false, 0, null);
+
+    touch.target.dispatchEvent(simulatedEvent);
+    event.preventDefault();
+}
+
+function init() {
+    document.addEventListener("touchstart", touchHandler, true);
+    document.addEventListener("touchmove", touchHandler, true);
+    document.addEventListener("touchend", touchHandler, true);
+    document.addEventListener("touchcancel", touchHandler, true);
+}
+
+$(document).ready(function() {
+	function int();
+});
