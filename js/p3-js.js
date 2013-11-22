b59422802e81e43a6b854faac120f6ece257cd7d
Shawn Roe <roe.shawn@gmail.com>
2013-11-22 18:52:04 +0900

    Added remove button next to food item that removes the li item and recalulates the total calories

--- js/p3-js.js
+++ js/p3-js.js
@@ -7,10 +7,24 @@
  *	http://p3.kunostories.biz
 --------------------------------------------------*/
 
-/*----------------------------------------------------------------------
-Pictures are draggable, adds total calories, and makes breakfast list
------------------------------------------------------------------------*/
 $(document).ready(function() {
+
+	/*----------------------------------------------------------------------
+	Remove button will remove breakfast item from list
+	-----------------------------------------------------------------------*/
+	$(document).on('click', 'button.remove', function() {
+		// add up the total calories
+		var totalCalories = parseInt($('#totalCalories').text());
+		var foodCalories = parseInt($(this).parent().data('calories'));
+		$('#totalCalories').text(totalCalories - foodCalories);
+
+		$(this).parent().remove();
+	});
+
+
+	/*----------------------------------------------------------------------
+	Pictures are draggable, adds total calories, and makes breakfast list
+	-----------------------------------------------------------------------*/
 	var contents = '';
 	var calories = '';
 	var servingsize = '';
@@ -29,28 +43,31 @@ $(document).ready(function() {
 	$('#right').droppable( {
 		accept: 'img',
 		drop: function() {
+
+			// if the food item exists, add the serving sizes
 			if($('#plate > li:contains(' + contents + ')').length > 0) {
 				var currentServingsize = parseFloat($('#plate > li:contains(' + contents + ')').data('serving-size'));
 				var newServingsize = parseFloat(currentServingsize + servingsize);
-				$('#plate > li:contains(' + contents + ')').replaceWith('<li data-serving-size="' + newServingsize + '">' + contents + ' (' + newServingsize + ' ' + unit + ')</li>');
-
-
+				var currentCalories = parseFloat($('#plate > li:contains(' + contents + ')').data('calories'));
+				var newCalories = parseFloat(currentCalories + calories);
+				$('#plate > li:contains(' + contents + ')').replaceWith('<li data-calories="' + newCalories + '" data-serving-size="' + newServingsize + '">' + contents + ' (' + newServingsize + ' ' + unit + ') <button class="remove">remove</button></li>');
 			}
+
+			// add the food item to the list
 			else {
-				$('#plate').append('<li data-serving-size="' + servingsize + '">' + contents + ' (' + servingsize + ' ' + unit + ')</li>');
+				$('#plate').append('<li data-calories="' + calories + '" data-serving-size="' + servingsize + '">' + contents + ' (' + servingsize + ' ' + unit + ') <button class="remove">remove</button></li>');
 			}
 
+			// add up the total calories
 			var totalCalories = parseInt($('#totalCalories').text());
 			var foodCalories = parseInt(calories);
 			$('#totalCalories').text(totalCalories + foodCalories);
 		}
 	});
-});
 
-/*----------------------------------------------------------------------
-Fruit section is accordion
------------------------------------------------------------------------*/
-$(document).ready(function() {
+	/*----------------------------------------------------------------------
+	Fruit section is accordion
+	-----------------------------------------------------------------------*/
 	$("#food").accordion( {
 		collapsible: true,
 	    heightStyle: "content"
